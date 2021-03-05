fa7ae21635d894be6cd98901eb6b668a80304449
Marco Antonio <79797841+marcofeliponi@users.noreply.github.com>
2021-03-05 14:03:39 -0300

    first commit

---
+++ URImedia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace URImedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int H, KMH;
+            double L;
+
+            H = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            KMH = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            L = (H * KMH) / 12.0;
+
+            Console.WriteLine(L.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
